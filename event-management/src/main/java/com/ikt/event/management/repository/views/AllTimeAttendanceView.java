66809902303d8039d40f8829e06f78299cc116fa
ivicacickoski <ivica.cickoski@inellipse.com>
2024-03-21 01:36:31 +0100

    all time attendance view

---
+++ event-management/src/main/java/com/ikt/event/management/repository/views/AllTimeAttendanceView.java
@@ -0,0 +1,15 @@
+package com.ikt.event.management.repository.views;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AllTimeAttendanceView {
+    private String name;
+    private Long totalAttendance;
+}
