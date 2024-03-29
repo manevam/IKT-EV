a3d3978f077fe129139e129e6c545aed3e8e8818
sara <andrioska.sara@gmail.com>
2024-03-29 22:56:02 +0100

    created header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const header = (props) => {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-md navbar-fixed" style={{ backgroundColor: "#e3f2fd" }}>
+                <div class="container">
+                    <a className="navbar-brand" href="/">Event Management</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse"
+                        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarCollapse">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to={"/users"}>Users</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default header;
