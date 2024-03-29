294e03f6ed8bae2a98f175649e21d3966b18024a
sara <andrioska.sara@gmail.com>
2024-03-29 18:56:00 +0100

    initialized project

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+  componentDidMount() {
+  }
+
+}
+
+export default App;
