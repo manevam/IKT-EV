2f8cb3f8e3e96306c1acf84628744ab4c89eb08f
andrioskasara <andrioska.sara@gmail.com>
2024-05-15 22:36:08 +0200

    implemented user registration for event

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,126 +1,140 @@
 import './App.css';
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Users from '../Users/UserList/users';
 import UserAdd from '../Users/UserAdd/userAdd';
 import UserService from '../../services/userService';
 import CompanyService from '../../services/companiesService';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Header from '../Header/header';
 import Companies from '../Companies/CompanyList/companies';
 import CompanyAdd from '../Companies/CompanyAdd/companyAdd';
-import CreateEvent from "../Events/EventAdd/CreateEvent";
 import Events from "../Events/EventList/events";
 import eventService from "../../services/eventService";
-import getAllCoordinators from "../Events/getAllCoordinators/getAllCoordinators";
-import data from "bootstrap/js/src/dom/data";
 import GetAllCoordinators from "../Events/getAllCoordinators/getAllCoordinators";
 import EventsPerCompany from "../Events/EventsPerCompany/eventsPerCompany";
 import Attendance from "../Events/EventAttendance/attendance";
 import EventAdd from "../Events/EventAdd/eventAdd";
-import img from "../../1692193299886.jpg"
+import UserRegisterForEvent from "../Users/UserRegisterForEvent/userRegisterForEvent";
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: [],
-      companies: [],
-      events: [],
-      coordinators: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [],
+            companies: [],
+            events: [],
+            coordinators: []
+        };
+    }
 
-  render() {
-    return (
-      <Router>
-        <Header />
-        <main>
-          <div className="container landing-page">
-            <Routes>
-              <Route path={"/users/add"} element={<UserAdd userAdd={this.state.users} onAddUser={this.addUser} companies={this.state.companies} />} />
-              <Route path={"/users"} element={<Users users={this.state.users} />} />
-              <Route path={"/companies/add"} element={<CompanyAdd companyAdd={this.state.companies} onAddCompany={this.addCompany} />} />
-              <Route path={"/companies"} element={<Companies companies={this.state.companies} />} />
-              <Route path={"/event"} element={<Events events={this.state.events} />} />
-              <Route path={"/event/create"} element={<EventAdd companies={this.state.companies} users={this.state.users} onAddEvent={this.addEvent} />} />
-              <Route path={"/event/coordinators"} element={<GetAllCoordinators coordinators={this.state.coordinators} />} />
-              <Route path={"/event/company"} element={<EventsPerCompany eventsPerCompany={this.state.eventsPerCompany} />} />
-              <Route path={"/event/attendance/:id"} element={<Attendance />} />
-            </Routes>
-          </div>
-        </main>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <Header/>
+                <main>
+                    <div className="container landing-page">
+                        <Routes>
+                            <Route path={"/users/add"}
+                                   element={<UserAdd userAdd={this.state.users} onAddUser={this.addUser}
+                                                     companies={this.state.companies}/>}/>
+                            <Route path={"/users/:userID/register"}
+                                   element={<UserRegisterForEvent onRegister={this.registerUserForEvent}
+                                                                  events={this.state.events}/>}/>
+                            <Route path={"/users"} element={<Users users={this.state.users}/>}/>
+                            <Route path={"/companies/add"} element={<CompanyAdd companyAdd={this.state.companies}
+                                                                                onAddCompany={this.addCompany}/>}/>
+                            <Route path={"/companies"} element={<Companies companies={this.state.companies}/>}/>
+                            <Route path={"/event"} element={<Events events={this.state.events}/>}/>
+                            <Route path={"/event/create"}
+                                   element={<EventAdd companies={this.state.companies} users={this.state.users}
+                                                      onAddEvent={this.addEvent}/>}/>
+                            <Route path={"/event/coordinators"}
+                                   element={<GetAllCoordinators coordinators={this.state.coordinators}/>}/>
+                            <Route path={"/event/company"}
+                                   element={<EventsPerCompany eventsPerCompany={this.state.eventsPerCompany}/>}/>
+                            <Route path={"/event/attendance/:id"} element={<Attendance/>}/>
+                        </Routes>
+                    </div>
+                </main>
+            </Router>
+        );
+    }
 
-  componentDidMount() {
-    this.fetchData()
-  }
+    componentDidMount() {
+        this.fetchData()
+    }
 
-  fetchData = () => {
-    this.loadUsers();
-    this.loadCompanies();
-    this.loadEvents();
-    this.loadCoordinators()
-  }
-
-  loadUsers = () => {
-    UserService.getUsers()
-      .then((data) => {
-        this.setState({
-          users: data.data
-        })
-      });
-  }
-
-  addUser = (personName, personEmail, phoneNumber, companyName) => {
-    UserService.addUser(personName, personEmail, phoneNumber, companyName)
-      .then(() => {
+    fetchData = () => {
         this.loadUsers();
-      })
-  }
-
-  loadCompanies = () => {
-    CompanyService.getCompanies()
-      .then((data) => {
-        this.setState({
-          companies: data.data
-        })
-      });
-  }
-
-  loadEvents = () => {
-    eventService.getAllEvents()
-        .then((data)=>{
-          this.setState({
-            events: data.data
-          })
-        });
-  }
-
-  loadCoordinators = () => {
-    eventService.getAllCoordinators()
-        .then((data) => {
-          this.setState({
-            coordinators: data.data
-          })
-        });
-  }
-
-  addEvent = (event) => {
-    eventService.createEvent(event)
-        .then(()=>{
-          this.loadEvents();
-        })
-  }
-
-  addCompany = (companyName, companyEmail) => {
-    CompanyService.addCompany(companyName, companyEmail)
-      .then(() => {
         this.loadCompanies();
-      })
-  }
+        this.loadEvents();
+        this.loadCoordinators()
+    }
+
+    loadUsers = () => {
+        UserService.getUsers()
+            .then((data) => {
+                this.setState({
+                    users: data.data
+                })
+            });
+    }
+
+    addUser = (personName, personEmail, phoneNumber, companyName) => {
+        UserService.addUser(personName, personEmail, phoneNumber, companyName)
+            .then(() => {
+                this.loadUsers();
+            })
+    }
+
+    registerUserForEvent = (userID, eventId, role) => {
+        UserService.registerForEvent(userID, eventId, role)
+            .then(() => {
+                this.loadUsers();
+            });
+    }
+
+    loadCompanies = () => {
+        CompanyService.getCompanies()
+            .then((data) => {
+                this.setState({
+                    companies: data.data
+                })
+            });
+    }
+
+    loadEvents = () => {
+        eventService.getAllEvents()
+            .then((data) => {
+                this.setState({
+                    events: data.data
+                })
+            });
+    }
+
+    loadCoordinators = () => {
+        eventService.getAllCoordinators()
+            .then((data) => {
+                this.setState({
+                    coordinators: data.data
+                })
+            });
+    }
+
+    addEvent = (event) => {
+        eventService.createEvent(event)
+            .then(() => {
+                this.loadEvents();
+            })
+    }
+
+    addCompany = (companyName, companyEmail) => {
+        CompanyService.addCompany(companyName, companyEmail)
+            .then(() => {
+                this.loadCompanies();
+            })
+    }
 }
 
 export default App;
---
+++ frontend/src/components/Users/UserRegisterForEvent/userRegisterForEvent.js
@@ -0,0 +1,120 @@
+import React, {useState} from "react";
+import {useParams, Link} from 'react-router-dom';
+
+const UserRegisterForEvent = ({onRegister, events}) => {
+
+    const {userID} = useParams();
+    const [selectedEvent, setSelectedEvent] = useState('');
+    const [selectedRole, setSelectedRole] = useState('');
+    const [message, setMessage] = useState('');
+    const [formData, setFormData] = useState({
+        personName: '',
+        personEmail: '',
+        phoneNumber: ''
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        if (name === 'event') {
+            setSelectedEvent(value);
+        } else if (name === 'role') {
+            setSelectedRole(value);
+        } else {
+            setFormData({
+                ...formData,
+                [name]: value.trim()
+            });
+        }
+    };
+
+    const OnFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!selectedEvent || !selectedRole) {
+            setMessage('Please select both event and role.');
+            return;
+        }
+
+        onRegister(userID, selectedEvent, selectedRole)
+            .then(() => {
+                setMessage('User registered for event successfully.');
+                setFormData({
+                    personName: '',
+                    personEmail: '',
+                    phoneNumber: ''
+                });
+                setSelectedEvent('');
+                setSelectedRole('');
+            })
+            .catch((error) => {
+                setMessage(`Error: ${error.message}`);
+            });
+    };
+
+
+    return (
+        <div className="row mt-5">
+            <div className="col-md-5">
+                <form onSubmit={OnFormSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="personName">Name</label>
+                        <input type="text"
+                               className="form-control"
+                               id="personName"
+                               name="personName"
+                               required
+                               onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="personEmail">Email</label>
+                        <input type="text"
+                               className="form-control"
+                               id="personEmail"
+                               name="personEmail"
+                               required
+                               onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="phoneNumber">Phone Number</label>
+                        <input type="text"
+                               className="form-control"
+                               id="phoneNumber"
+                               name="phoneNumber"
+                               required
+                               onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="event">Events</label>
+                        <select name="event" className="form-control" onChange={handleChange}>
+                            {events.map((event) => (
+                                <option key={event.id} value={event.id}>
+                                    {event.name} by {event.company.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="role">Role</label>
+                        <select name="role" className="form-control" onChange={handleChange} value={selectedRole}>
+                            <option value="USER">User</option>
+                            <option value="ADMIN">Admin</option>
+                        </select>
+                    </div>
+
+                    <button id="submit" type="submit" className="btn btn-success mt-3">Register to Event</button>
+                    <Link to={"/users"} className={"btn btn-block btn-primary ms-3 mt-3"}>Back to users</Link>
+                </form>
+            </div>
+            {message && <p>{message}</p>}
+        </div>
+    )
+}
+
+export default UserRegisterForEvent;
